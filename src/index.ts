5c592c8b5c39c4411cddf5ab583d5aac9651704c
coding-zero-I <yashsangwan4700@gmail.com>
2025-06-11 12:51:06 +0530

    added cookie parser and cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { config } from "dotenv";
 import authRouter from "./routes/authRoutes";
 import connectToDb from "./db/connectToDb";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 config();
 
 const app = express();
@@ -14,6 +16,11 @@ connectToDb().then(() => {
 });
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:3000", // Replace with your client URL
+  credentials: true, // Allow cookies to be sent with requests
+}));
 app.use("/api/v1/auth",authRouter);
 
 app.listen(process.env.PORT || 3000, () => {
