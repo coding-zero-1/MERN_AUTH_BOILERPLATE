ff31b274bfe5bf051a96aac7049b5cb743764615
coding-zero-I <yashsangwan4700@gmail.com>
2025-06-11 12:38:36 +0530

    added util function to generate verification token

---
+++ src/utils/generateVerificationCode.ts
@@ -0,0 +1,11 @@
+const generateVerificationCode = (length: number): string => {
+    const characters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789';
+    let code = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        code += characters[randomIndex];
+    }
+    return code;
+}
+
+export default generateVerificationCode;
